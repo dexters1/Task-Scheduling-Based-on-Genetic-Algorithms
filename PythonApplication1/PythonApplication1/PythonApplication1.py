0a58e28114b763e8957c987e262583fea9846130
student <student@domain.local>
2019-04-02 15:07:54 +0200

    graf gotov

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -8,15 +8,21 @@ if __name__ == "__main__":
     print([x.first.val for x in G.E])
     print([x.second.val for x in G.E])
 
-    Lin = get_in_degrees(G)
-    Lout = get_out_degrees(G)
+    Lin = get_in_degrees(G, G.V[0])
+    Lout = get_out_degrees(G, G.V[0])
     Lsuc = []
-    get_succesors(G.V[0], G, Lsuc)
+    get_succesors(G.V[7], G, Lsuc)
     Lpred = []
-    get_predecessors(G.V[8], G, Lpred)
+    get_predecessors(G.V[1], G, Lpred)
 
-    for i in range(len(G.V)):
-        print("Node:", G.V[i].val, "In degrees:", Lin[i], "Out degrees:", Lout[i])
-    print("Succesors: ", Lsuc)
-    print("Predecessors: ", Lpred)
+    print("Succesors: ", [x.val for x in Lsuc])
+    print("Predecessors: ", [x.val for x in Lpred])
+    print("In degrees: ", [x.val for x in Lin])
+    print("Out degrees: ", [x.val for x in Lout])
+    
+    preProcessing(G)
+    print("\nValues after preProcessing(): \n")
+    print([x.val for x in G.V])
+    print([x.first.val for x in G.E])
+    print([x.second.val for x in G.E])
     
--- PythonApplication1/PythonApplication1/functions.py
+++ PythonApplication1/PythonApplication1/functions.py
@@ -32,32 +32,28 @@ def make_graph():
 
     return Graph(vertex, edges)
 
-def get_in_degrees(G):
+def get_in_degrees(G, v):
     L = []
-    for v in G.V:
-        n = 0
-        for edge in G.E:
-            if edge.second == v:
-                n += 1
-        L.append(n)
+    n = 0
+    for edge in G.E:
+        if edge.second == v:
+                L.append(edge.first)
     return L
 
-def get_out_degrees(G):
+def get_out_degrees(G, v):
     L = []
-    for v in G.V:
-        n = 0
-        for edge in G.E:
-            if edge.first == v:
-                n += 1
-        L.append(n)
+    n = 0
+    for edge in G.E:
+        if edge.first == v:
+            L.append(edge.second)
     return L
 
 def get_succesors(node, G, L):
     S = []
     for edge in G.E:
        if edge.first == node:
-         if edge.second.val not in L:
-             L.append(edge.second.val)
+         if edge.second not in L:
+             L.append(edge.second)
              S.append(edge.second)
     for suc in S:
         get_succesors(suc, G, L)
@@ -67,9 +63,36 @@ def get_predecessors(node, G, L):
     S = []
     for edge in G.E:
        if edge.second == node:
-           if edge.first.val not in L:
-             L.append(edge.first.val)
+           if edge.first not in L:
+             L.append(edge.first)
              S.append(edge.first)             
     for pred in S:
         get_predecessors(pred, G, L)
     return L
+
+def preProcessing(G):
+    cG = G.V[:]
+    for v in cG:
+        i = get_in_degrees(G, v)
+        o = get_out_degrees(G, v)
+        print("in_deg: ", len(i), "out_deg", len(o))
+        #if (len(i) <= 1) and (len(o) == 1):
+        if (len(o) == 1):
+               iChild = get_in_degrees(G, o[0])
+               if(len(iChild) == 1):
+                   combineNode(v, o[0], G)
+
+
+def combineNode(v, o, G): 
+    iV = get_in_degrees(G, v)
+    #if (len(iV) == 1 ):
+    #    G.E.append(Edge(iV[0],o))
+    for iter in iV:
+        G.E.append(Edge(iter,o))
+    G.V.remove(v)
+    cG = G.E[:]
+    for iter in cG:
+        if iter.first == v or iter.second == v:
+            G.E.remove(iter)
+            
+       
